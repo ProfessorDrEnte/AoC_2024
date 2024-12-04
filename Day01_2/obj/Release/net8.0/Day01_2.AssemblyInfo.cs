d9d58a9b3916f24fb6844315ae94e11965b3e8cd
Prof. Dr. Ente <95550209+ProfessorDrEnte@users.noreply.github.com>
2024-12-04 20:21:12 +0100

    Day01_Part2

--- Day01_2/obj/Release/net8.0/Day01_2.AssemblyInfo.cs
+++ Day01_2/obj/Release/net8.0/Day01_2.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Day01")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Day01_2")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6f634e871175f51531665218ca02c3022e40244")]
-[assembly: System.Reflection.AssemblyProductAttribute("Day01")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Day01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("Day01_2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Day01_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Von der MSBuild WriteCodeFragment-Klasse generiert.
